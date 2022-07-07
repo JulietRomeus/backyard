fefc40a751f62cb291baee6409c0bdd179e4b363
aom.chatchalerm <aom.chatchalerm@gmail.com>
2022-07-07 18:01:15 +0700

    cors-dot-dev

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       'http://localhost:3100',
       'http://localhost:9000',
       'https://bigdata.rtarf.maholan.app',
+      'https://bigdata.rtarf.maholan.dev',
     ];
     app.enableCors({
       origin: (origin, callback) => {
