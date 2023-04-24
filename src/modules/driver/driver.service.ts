1478c83e2ec341a94bada7a13c2ef0be8d502395
paweenwat prateeprat <pawinwat_wat@hotmail.com>
2023-04-25 00:03:41 +0700

    import

--- src/modules/driver/driver.service.ts
+++ src/modules/driver/driver.service.ts
@@ -1,3 +1,6 @@
+import { Roles } from './../../decorators/roles.decorator';
+import { Role } from './../../entities/role.entity';
+import { format, parse } from 'date-fns';
 import { Injectable } from '@nestjs/common';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
@@ -11,7 +14,7 @@ import {
 import { User } from './../../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { find } from 'rxjs';
+import { find, firstValueFrom } from 'rxjs';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import now from '../../utils/now';
 import genPayload, {
@@ -20,6 +23,9 @@ import genPayload, {
   ForbiddenException,
 } from 'src/utils/payload';
 import { trsActivityVehicleDriver } from '../../entities/Index';
+import { Unit } from 'src/entities/unit.entity';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class DriverService {
   constructor(
@@ -31,15 +37,22 @@ export class DriverService {
     private trsDrivingLicenseTypeRepo: Repository<trsDrivingLicenseType>,
     @InjectRepository(User, 'PROGRESS')
     private userRepo: Repository<User>,
-    @InjectRepository(trsDriverTemplate,'MSSQL_CONNECTION')
+    @InjectRepository(trsDriverTemplate, 'MSSQL_CONNECTION')
     private templateRepo: Repository<trsDriverTemplate>,
-    @InjectRepository(trsActivityVehicleDriver,'MSSQL_CONNECTION')
+    @InjectRepository(trsActivityVehicleDriver, 'MSSQL_CONNECTION')
     private trsActivityVehicleDriverRepo: Repository<trsDriverTemplate>,
+    @InjectRepository(Unit, 'PROGRESS')
+    private UnitRepo: Repository<Unit>,
+    @InjectRepository(Role,'PROGRESS')
+    private roleRepo:Repository<Role>,
+    private readonly httpService: HttpService,
+    private readonly configService:ConfigService
 
-  ) {}
+
+  ) { }
 
   async create(createDriverDto: any) {
-    console.log('createDriverDtoooooo', createDriverDto);
+    // console.log('createDriverDtoooooo', createDriverDto);
     const actionType = ACTIONTYPE.CREATE;
     let timeNow = now();
     let user = createDriverDto.request_by;
@@ -63,16 +76,17 @@ export class DriverService {
       createObj[keys] = dataObj[keys] || null;
     });
     createObj.is_active = true;
-    createObj.trs_driver_license_lists = trs_driver_license_lists;
+    if (trs_driver_license_lists) createObj.trs_driver_license_lists = trs_driver_license_lists;
+    // console.log('createObj',createObj)
     const dbRes = await this.trsDriverRepo.save(createObj);
     return dbRes;
   }
 
   async findAll(body: any) {
-    console.log('body', body);
+    // console.log('body', body);
     const unit_no = body.request_by.units?.map((r: any) => `'${r.code}'`);
-    console.log(unit_no);
-    let data =  await this.trsDriverRepo
+    // console.log(unit_no);
+    let data = await this.trsDriverRepo
       .createQueryBuilder('d')
       .where(`d.is_active = 1 and d.unit_no in (${unit_no})`)
       .leftJoinAndSelect(
@@ -83,27 +97,27 @@ export class DriverService {
       .leftJoinAndSelect('d.driver_status', 'tds')
       .leftJoin('d.trs_activity_vehicle_drivers', 'tavs')
       .addSelect('tavs.id')
-      .leftJoin('tavs.activity', 'tavsa')
+      .leftJoin('tavs.activity', 'tavsa', 'tavsa.is_delete =0')
       .addSelect('tavsa.id')
       .addSelect('tavsa.activity_start_date')
       .addSelect('tavsa.activity_end_date')
       // .getQuery();
       .getMany();
-      const finalItem = data.map(rec=>({
-        ...rec,
-        is_busy:rec.trs_activity_vehicle_drivers.some(r=>r?.activity?.is_inprogress) || false
-      }))
-      return finalItem
+    const finalItem = data.map(rec => ({
+      ...rec,
+      is_busy: rec.trs_activity_vehicle_drivers.some(r => r?.activity?.is_inprogress) || false
+    }))
+    return finalItem
   }
 
 
   async findBusy(body) {
 
     return await this
-    .trsActivityVehicleDriverRepo.createQueryBuilder('atd')
-    .leftJoinAndSelect('atd.activity','a','a.is_delete = 0')
-    .leftJoinAndSelect('atd.driver','d','d.is_active = 1')
-    
+      .trsActivityVehicleDriverRepo.createQueryBuilder('atd')
+      .leftJoinAndSelect('atd.activity', 'a', 'a.is_delete = 0')
+      .leftJoinAndSelect('atd.driver', 'd', 'd.is_active = 1')
+
       .getMany();
   }
 
@@ -160,7 +174,7 @@ export class DriverService {
 
   async findOne(id: any) {
     console.log(id);
-    const data =  await this.trsDriverRepo
+    const data = await this.trsDriverRepo
       .createQueryBuilder('d')
       .leftJoinAndSelect(
         'd.trs_driver_license_lists',
@@ -171,17 +185,17 @@ export class DriverService {
       .leftJoinAndSelect('d.driver_status', 'tds')
       .leftJoin('d.trs_activity_vehicle_drivers', 'tavs')
       .addSelect('tavs.id')
-      .leftJoin('tavs.activity', 'tavsa')
+      .leftJoin('tavs.activity', 'tavsa', 'tavsa.is_active =1')
       .addSelect('tavsa.id')
       .addSelect('tavsa.activity_start_date')
       .addSelect('tavsa.activity_end_date')
       // .getQuery();
-      .getOne();      
-        let finalItem:any =data
-        finalItem.is_busy =data?.trs_activity_vehicle_drivers?.some(r=>r?.activity?.is_inprogress) || false
-    
-     
-      return data
+      .getOne();
+    let finalItem: any = data
+    finalItem.is_busy = data?.trs_activity_vehicle_drivers?.some(r => r?.activity?.is_inprogress) || false
+
+
+    return data
   }
 
   async update(id: any, updateDriverDto: any) {
@@ -233,9 +247,129 @@ export class DriverService {
     return db;
   }
 
-  async getTemplate(){
-    return await this.templateRepo.find({ where:{is_active:true}})
+  async getTemplate() {
+    return await this.templateRepo.find({ where: { is_active: true } })
   }
+
+
+  async importDriver(req,body) {
+    const formatString = 'ddMMyyyy';
+    const unitOption = await this.UnitRepo.find()
+    const dirverId = this.configService.get('DRIVER_ROLE_ID')
+    // console.log(req?.headers?.authorization)
+
+    const object = body.data.map(async (rec) => {
+
+      rec.request_by = body.request_by
+      rec.id_card = String(rec?.id_card)
+      rec.unit_no = String(rec?.unit_no)
+      // rec.unit_code = String(rec?.unit_no)
+
+      // delete rec?.unit_no
+      rec.driver_name = rec.firstname + ' ' + rec.lastname
+
+      let user:any = {}
+      let haveUser = false
+      if (rec?.id_card){
+        user = await this.userRepo.findOne({
+          where:{
+            // status:1,
+            idCard:rec?.id_card
+          },
+          relations:['roles']
+        })
+
+      }
+
+      // .createQueryBuilder('us')
+      // .where(`us.idCard is not null and us.idCard = ${rec?.idp}`).getOne()
+
+
+      if (user?.id && rec?.id_card ) haveUser = true
+      // console.log('have user')
+
+      console.log('user',user,rec?.id_card)
+
+      //if user already exists
+      if (haveUser) {
+        console.log('update user')
+
+        // add driver id to user
+        rec.driver_id = user.id
+        user.activeUnit = unitOption.find(r=>r.code===rec.unit_no)
+        user.status=1
+        const driverRole = new Role()
+        driverRole.id = dirverId
+        user.roles.push(driverRole)
+        this.userRepo.save(user)
+        // add role driver to user
+        // 41f9b763-6d79-4927-98ee-f2abb37f99b5
+        //set user's active unit
+        // const res = await this.userRepo.save(user)
+
+      }
+      else if (rec?.email){
+        console.log('create user with email',rec?.email)
+        
+        //create user
+        let cUser = new User()
+        // let unit = new Unit()
+        let tempUnit = unitOption.find(r=>r.code===rec.unit_no)
+        cUser.activeUnit = this.UnitRepo.create({...tempUnit})
+        cUser.units = this.UnitRepo.create([tempUnit])
+        cUser.username = rec.email
+        cUser.email = rec.email
+        cUser.firstname = rec?.firstname  || ''
+        cUser.lastname = rec?.lastname  || ''
+        cUser.status = 1
+        cUser.password = '$2b$10$KSa3KWogEEbDTV0Nh5OlmuTtjz.VivJ.Ls5F03PQqb5.44d4Gc.Yy'
+        cUser.idCard = rec.id_card
+        // cUser.id = user.id
+        const driverRole = new Role()
+        driverRole.id = dirverId
+        cUser.roles = [driverRole]
+        console.log('cUser',cUser)
+
+        const res = await this.userRepo.save(cUser)
+        rec.driver_id = res.id
+        // let res = await firstValueFrom(this.httpService.post(`/user`,user))
+        console.log(res)
+        //
+      }
+      else{
+        console.log('no email provided')
+      }
+
+
+      const tempLicense = []
+
+
+      if (typeof rec?.license_type_id1 == 'number') tempLicense.push({
+        license_id: parseInt(rec?.license_type_id1),
+        ...(rec?.license_issue_date1 ? { issue_date: parse(rec?.license_issue_date1, formatString, new Date()) } : {}),
+        ...(rec?.license_expire_date1 ? { expire_date: parse(rec?.license_expire_date1, formatString, new Date()) } : {})
+
+      })
+      if (typeof rec?.license_type_id2 == 'number') tempLicense.push({
+        license_id: parseInt(rec?.license_type_id2),
+        ...(rec?.license_issue_date2 ? { issue_date: parse(rec?.license_issue_date2, formatString, new Date()) } : {}),
+        ...(rec?.license_expire_date2 ? { expire_date: parse(rec?.license_expire_date2, formatString, new Date()) } : {})
+      })
+
+      rec.trs_driver_license_lists = tempLicense
+      await this.create(rec)
+      return rec
+    })
+
+    return object
+    //Check account by idp
+    //case 1 Create account as Driver and unactiveUnit as unit
+    //case 2 add driver role,update licens,add activeUnit, unit,person_no, default pw 1234, ignore Email
+    // return await this.templateRepo.find({ where:{is_active:true}})
+  }
+
+
+
 }
 
 
